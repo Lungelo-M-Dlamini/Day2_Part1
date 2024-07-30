01fde2add3164d59408884054dea7f203dac83f0
Lungelo-M-Dlamini <neymardlamini@gmail.com>
2024-07-30 17:57:07 +0200

    Add project files.

---
+++ Day2_Part1/Program.cs
@@ -0,0 +1,58 @@
+﻿const int MaximumRed = 12;
+const int MaximumGreen = 13;
+const int MaximumBlue = 14;
+
+var Total = 0;
+string[] lines = File.ReadAllLines("C:\\Users\\Lungelo\\source\\repos\\Day2_Part1\\Day2_Part1\\Input.txt");
+
+foreach (var line in lines) 
+{
+    var gameInformation = line.Split(':');
+    var gameIdentity = int.Parse(gameInformation[0].Split(' ')[1]);
+    var rounds = gameInformation[1].Split(';', StringSplitOptions.TrimEntries);
+    bool gameValidation = true;
+    foreach (var round in rounds)
+    {
+        
+        var colorInformations = round.Split(",", StringSplitOptions.TrimEntries);
+        foreach(var color in colorInformations) 
+        {
+            var colorInformation = color.Split(' ');
+            var colorCount = int.Parse(colorInformation[0]);
+            var colorName = colorInformation[1];
+            switch (colorName) 
+            {
+                case "red":
+                    if(colorCount > MaximumRed) 
+                    {
+                        gameValidation = false;
+                    }
+                    break;
+                case "green":
+                    if (colorCount > MaximumGreen)
+                    {
+                        gameValidation = false;
+                    }
+                    break;
+                case "blue":
+                    if (colorCount > MaximumBlue)
+                    {
+                        gameValidation = false;
+                    }
+                    break;
+
+            }
+        }
+        if (!gameValidation) 
+        {
+            break;
+        }
+    }
+    if (gameValidation) 
+    {
+        Total += gameIdentity;
+    }
+}
+
+Console.WriteLine(Total);
+Console.ReadLine();
---
+++ Day2_Part2/Program.cs
@@ -0,0 +1,39 @@
+﻿var Total = 0;
+string[] lines = File.ReadAllLines("C:\\Users\\Lungelo\\source\\repos\\Day2_Part1\\Day2_Part2\\Input.txt");
+
+foreach (var line in lines)
+{
+    var gameInformation = line.Split(':');
+    var gameIdentity = int.Parse(gameInformation[0].Split(' ')[1]);
+    var rounds = gameInformation[1].Split(';', StringSplitOptions.TrimEntries);
+    var maximumRed = 0; var maximumGreen = 0; var maximumBlue = 0;
+    foreach (var round in rounds)
+    {
+
+        var colorInformations = round.Split(",", StringSplitOptions.TrimEntries);
+        foreach (var color in colorInformations)
+        {
+            var colorInformation = color.Split(' ');
+            var colorCount = int.Parse(colorInformation[0]);
+            var colorName = colorInformation[1];
+            switch (colorName)
+            {
+                case "red":
+                    maximumRed = Math.Max(colorCount, maximumRed);
+                    break;
+                case "green":
+                    maximumGreen = Math.Max(colorCount, maximumGreen);
+                    break;
+                case "blue":
+                    maximumBlue = Math.Max(colorCount, maximumBlue);
+                    break;
+
+            }
+        }
+    }
+    var ans = maximumRed * maximumGreen * maximumBlue;
+    Total += ans;
+}
+
+Console.WriteLine(Total);
+Console.ReadLine();
